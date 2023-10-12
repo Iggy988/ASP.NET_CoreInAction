7dc44003ee1232243b0dd2d79b3fffd68b689aed
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-12 11:26:29 +0200

    Adding multiple filters to the endpoint filter pipeline

--- MinimalWebAPI/Program.cs
+++ MinimalWebAPI/Program.cs
@@ -10,7 +10,14 @@ app.MapGet("/fruit", () => /*Fruit.All*/ _fruit);
 //var getFruit = (string id) => Fruit.All[id];
 app.MapGet("/fruit/{id}", /*getFruit*/ (string id) => 
             _fruit.TryGetValue(id, out var fruit) ? TypedResults.Ok(fruit) : Results.Problem(statusCode: 404))
-                        .AddEndpointFilter(ValidationHelper.ValidateId);
+                        .AddEndpointFilter(ValidationHelper.ValidateId)
+                        .AddEndpointFilter(async(context, next) =>
+                        {
+                                    app.Logger.LogInformation("Executing filter...");
+                                    object? result = await next(context);
+                                    app.Logger.LogInformation($"Handler result: {result}");
+                                    return result;
+                        });
 
 app.MapPost("/fruit/{id}", /*Handlers.AddFruit*/ (string id, Fruit fruit) =>
             _fruit.TryAdd(id, fruit)
