2a64a9abf1daa1e36e3a48d66565b47f3f2ec6df
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-17 14:10:45 +0200

    Binding simple values using [From] attributes

--- ModelBindingAndValidation/Program.cs
+++ ModelBindingAndValidation/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/products/{id}/paged", ([FromRoute] int id, [FromQuery] int page, [FromHeader(Name="")] int pageSize) => $"Recived id {id}, page{page}, pageSize {pageSize}");
 
 app.Run();
