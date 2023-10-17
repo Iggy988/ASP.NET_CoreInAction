507c4709e61e54ce42170eb46d997f0f4065cdf0
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-17 14:53:12 +0200

    Implementing TryParse in a custom type to allow parsing from route values

--- ModelBindingAndValidation/Program.cs
+++ ModelBindingAndValidation/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/products/{id}/paged", ([FromRoute] int id, [FromQuery] int page, [FromHeader(Name="")] int pageSize) => $"Recived id {id}, page{page}, pageSize {pageSize}");
+//app.MapGet("/products/{id}/paged", ([FromRoute] int id, [FromQuery] int page, [FromHeader(Name="")] int pageSize) => $"Recived id {id}, page{page}, pageSize {pageSize}");
+
+app.MapGet("/product/{id}", (ProductId) => $"Recived {id}");
 
 app.Run();
+
+
+readonly record struct ProductId(int id)
+{
+  public static bool TryParse(string? s, out ProductId result)
+  {
+    if(s is not null && s.StartsWith('p') && int.TryParse(s.AsSpan().Slice(1), out int id)))
+    {
+      result = new Product(id);
+      return true;
+    }
+    result = default;
+    return false;
+  }
+}
