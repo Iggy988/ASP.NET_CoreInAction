3ae9e2fe749f5a2f270e796d3f2331f708349a8f
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-19 15:17:56 +0200

    Using BindAsync for custom model binding

--- ModelBindingAndValidation/Program.cs
+++ ModelBindingAndValidation/Program.cs
@@ -37,7 +37,24 @@ app.MapGet("/upload", (IFormFileCollection files) =>
 });
 string StockWithDefaultValue(int id = 0) => $"Received {id}";
 
+app.MapPost("/sizes", (SizeDetails size) => $"Received {size}");
+
 app.Run();
+
+public record SizeDetails(double height, double width) 
+{
+  public static async ValueTask<SizeDetails?> BindAsync(HttpContext context) 
+  {
+    using var sr = new StreamReader(context.Request.Body); 
+    string? line1 = await sr.ReadLineAsync(context.RequestAborted); 
+    if (line1 is null) { return null; } 
+    string? line2 = await sr.ReadLineAsync(context.RequestAborted); 
+    if (line2 is null) { return null; } 
+    return double.TryParse(line1, out double height) && double.TryParse(line2, out double width) 
+      ? new SizeDetails(height, width) 
+      : null; 
+  }
+}
 
 public interface IFormFile
 {
