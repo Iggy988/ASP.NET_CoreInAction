6e7b90e7dea37cc740136bb1b1e920e8f1dc396a
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-23 11:15:36 +0200

    Sending an email without DI when there are no dependencies

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -3,4 +3,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/register/{username}", RegisterUser);
+
 app.Run();
+
+
+string RegisterUser(string username)
+{
+  var emailSender = new EmailSender()
+  emailSender.SendEmail(username),;
+  return $"Email sent to {username}";
+}
+
+public classEmailSender
+{
+  public void SendEmail(string username)
+  {
+    Console.WriteLine($"Email sent to {username}!");
+  }
+}
