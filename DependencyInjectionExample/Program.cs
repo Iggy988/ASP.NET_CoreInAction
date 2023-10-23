276f911636573d0a4e4e8d94f673bd20fa99b1de
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-23 11:41:41 +0200

    Sending email without DI when you create dependencies manually

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -10,15 +10,32 @@ app.Run();
 
 string RegisterUser(string username)
 {
-  var emailSender = new EmailSender()
-  emailSender.SendEmail(username),;
-  return $"Email sent to {username}";
+  var emailSender = new EmailSender(
+    new MessageFactory(), new NetworkClient(new EmailServerSettings(Host: "smtp.server.com",Port: 25 )));
+  emailSender.SendEmail(username); 
+  return $"Email sent to {username}!";
 }
 
 public classEmailSender
 {
+  private readonly NetworkClient _client;
+  private readonly MessageFactory _factory; 
+  public EmailSender(MessageFactory factory, NetworkClient client); 
+
+  
   public void SendEmail(string username)
   {
+    var email = _factory.Create(username); 
+    _client.SendEmail(email);
     Console.WriteLine($"Email sent to {username}!");
+  }
+}
+
+public class NetworkClient
+{
+  private readonly EmailServerSettings _settings;
+  public NetworkClient(EmailServerSettings settings)
+  {
+    _settings = settings;
   }
 }
