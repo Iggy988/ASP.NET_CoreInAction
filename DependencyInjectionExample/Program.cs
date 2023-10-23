938d0ac24ee33caca9584dceb7f950b5b9aa5144
Iggy988 <igor.rodic1988@gmail.com>
2023-10-23 22:49:36 +0200

    updates

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -19,26 +19,3 @@ public interface IEmailSender
   public void SendEmail(string username);
 }
 
-public classEmailSender
-{
-  private readonly NetworkClient _client;
-  private readonly MessageFactory _factory; 
-  public EmailSender(MessageFactory factory, NetworkClient client); 
-
-  
-  public void SendEmail(string username)
-  {
-    var email = _factory.Create(username); 
-    _client.SendEmail(email);
-    Console.WriteLine($"Email sent to {username}!");
-  }
-}
-
-public class NetworkClient
-{
-  private readonly EmailServerSettings _settings;
-  public NetworkClient(EmailServerSettings settings)
-  {
-    _settings = settings;
-  }
-}
