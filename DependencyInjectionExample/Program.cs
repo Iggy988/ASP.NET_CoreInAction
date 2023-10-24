10df2a144a53e21c7c92f00dadc59dd4d53b69a0
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-24 13:47:54 +0200

    Update Program.cs

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -1,54 +1,91 @@
-builder.Services.AddScoped( provider => new EmailServerSettings ( 
-  Host: "smtp.server.com", 
-  Port: 25 
-));
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+// Can also remove the explicit configurations below and use extension method
+//builder.Services.AddEmailSender();
+
+//builder.Services.AddScoped<IEmailSender, EmailSender>();
+builder.Services.AddSingleton<NetworkClient>();
+builder.Services.AddScoped<MessageFactory>();
+builder.Services.AddSingleton(provider =>
+    new EmailServerSettings
+    (
+        Host: "smtp.server.com",
+        Port: 25
+    ));
 
 var app = builder.Build();
 
-
-
-app.MapGet("/", () => "Hello World!");
-
 app.MapGet("/register/{username}", RegisterUser);
-
-app.MapGet("/links", (LinkGenerator links) => 
-{
-  string link = links.GetPathByName("products");
-  return $"View the product at {link}";
-});
-
-app.MapRazorPages();
+app.MapGet("/", () => "Navigate to register/{username} to test the Mock email sending");
 
 app.Run();
 
-
-string RegisterUser(string username, IEmailSender emailSender) 
+string RegisterUser(string username, IEmailSender emailSender)
 {
-  emailSender.SendEmail(username); 
-  return $"Email sent to {username}!"; 
+    emailSender.SendEmail(username);
+    return $"Email sent to {username}!";
 }
 
+public record Email(string Address, string Message);
+public record EmailServerSettings(string Host, int Port);
 public interface IEmailSender
 {
-  public void SendEmail(string username);
+    void SendEmail(string username);
+}
+public class EmailSender: IEmailSender
+{
+    private readonly NetworkClient _client;
+    private readonly MessageFactory _factory;
+
+    public EmailSender(MessageFactory factory, NetworkClient client)
+    {
+        _factory = factory;
+        _client = client;
+    }
+
+    public void SendEmail(string username)
+    {
+        var email = _factory.Create(username);
+        _client.SendEmail(email);
+        Console.WriteLine($"Email sent to {username}!");
+    }
+}
+
+public class NetworkClient
+{
+    private readonly EmailServerSettings _settings;
+
+    public NetworkClient(EmailServerSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public void SendEmail(Email email)
+    {
+        Console.WriteLine($"Connecting to server {_settings.Host}:{_settings.Port}");
+        Console.WriteLine($"Email sent to {email.Address}: {email.Message}");
+    }
+}
+
+public class MessageFactory
+{
+    public Email Create(string emailAddress)
+        => new Email(emailAddress, "Thanks for signing up!");
 }
 
 public static class EmailSenderServiceCollectionExtensions
 {
-  public static IServiceCollection AddEmailSender(
-    this IServiceCollection services) 
-  {
-    services.AddScoped<IEmailSender, EmailSender>(); 
-    services.AddSingleton<NetworkClient>(); 
-    services.AddScoped<MessageFactory>(); 
-    services.AddSingleton(
-      new EmailServerSettings ( 
-        host: "smtp.server.com", 
-        port: 25 ));
-return services; #C
-  }
+    public static IServiceCollection AddEmailSender(this IServiceCollection services)
+    {
+        services.AddScoped<IEmailSender, EmailSender>();
+        services.AddSingleton<NetworkClient>();
+        services.AddScoped<MessageFactory>();
+        services.AddSingleton(
+            new EmailServerSettings
+            (
+                Host: "smtp.server.com",
+                Port: 25
+            ));
+        return services;
+    }
 }
