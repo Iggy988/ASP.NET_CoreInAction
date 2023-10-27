6b632d6dc5c6b684819b2900134870b04d400a69
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-27 12:59:49 +0200

    Resolving a scoped service using IServiceScope

--- LifetimeExample/Program.cs
+++ LifetimeExample/Program.cs
@@ -1,10 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<DataContext>();
 var app = builder.Build();
+
+var settings = app.Services.GetRequiredService<EmailServerSettings>();
 
 builder.Host.UseDefaultServiceProvider(o => 
 {
   o.ValidateScopes = true;
   o.ValidateOnBuild = true; 
 });
+
+await using (var scope = app.Services.CreateAsyncScope()) 
+{
+  var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>(); 
+  Console.WriteLine($"Retrieved scope: {dbContext.RowCount}");
+}
 
 app.Run();
