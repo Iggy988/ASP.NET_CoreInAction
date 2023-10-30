60fa315eac3eca04bb436d4767335923a56d4123
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-30 12:25:52 +0100

    Loading appsettings.json by clearing the configuration sources

--- StoreViewerApplication/Program.cs
+++ StoreViewerApplication/Program.cs
@@ -1,6 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.Source.Clear();
+builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => app.Configuration.AsEnumerable());
 
 app.Run();
