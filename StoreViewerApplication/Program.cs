328a7912c60b40a56fba27c0b7a850e9d4a5968a
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-30 12:51:30 +0100

    Loading from multiple providers in Program.cs

--- StoreViewerApplication/Program.cs
+++ StoreViewerApplication/Program.cs
@@ -1,7 +1,13 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.Source.Clear();
+builder.Configuration.Sources.Clear();
+builder.Configuration.AddJsonFile("sharedSsettings.json", optional: true); 
 builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+builder.Configuration.AddEnvironmentVariables();
+
+var zoomLevel = builder.Configuration["MapSettings:DefaultZoomLevel"];
+//var lat = builder.Configuration["MapSettings:DefaultLocation:Latitude"];
+var lat = builder.Configuration.GetSection("MapSettings")["DefaultLocation:Latitude"];
 
 var app = builder.Build();
 
