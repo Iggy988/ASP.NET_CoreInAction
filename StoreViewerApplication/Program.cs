7b7e84da3073e83c983e76109094dc1cacbf9b54
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-10-30 13:35:17 +0100

    User secrets

--- StoreViewerApplication/Program.cs
+++ StoreViewerApplication/Program.cs
@@ -9,6 +9,13 @@ var zoomLevel = builder.Configuration["MapSettings:DefaultZoomLevel"];
 //var lat = builder.Configuration["MapSettings:DefaultLocation:Latitude"];
 var lat = builder.Configuration.GetSection("MapSettings")["DefaultLocation:Latitude"];
 
+//dotnet user-secrets init
+//dotnet user-secrets set "MapSettings:GoogleMapsApiKey" F5RJT9GFHKR7
+if (builder.Environment.IsDevelopment())
+{
+  builder.Configuration.AddUserSecrets<Program>();
+}
+
 var app = builder.Build();
 
 app.MapGet("/", () => app.Configuration.AsEnumerable());
