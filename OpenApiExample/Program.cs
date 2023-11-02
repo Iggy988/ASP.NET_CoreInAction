8ebfce9f4f8e639a3e6200b294b5e5bef7819986
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-11-02 15:00:24 +0100

    Adding OpenAPI metadata to improve endpoint documentation

--- OpenApiExample/Program.cs
+++ OpenApiExample/Program.cs
@@ -15,11 +15,19 @@ WebApplication app = builder.Build();
 var _fruit = new ConcurrentDictionary<string, Fruit>();
 app.UseSwagger(); 
 app.UseSwaggerUI(); 
-app.MapGet("/fruit/{id}", (string id) => _fruit.TryGetValue(id, out var fruit) ? TypedResults.Ok(fruit) : Results.Problem(statusCode: 404));
+
+app.MapGet("/fruit/{id}", (string id) => _fruit.TryGetValue(id, out var fruit) ? TypedResults.Ok(fruit) : Results.Problem(statusCode: 404))
+  .WithTags("fruit")
+  .Produces<Fruit>()
+  .ProducesProblem(404);
+
 app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
 _fruit.TryAdd(id, fruit) ? TypedResults.Created($"/fruit/{id}", fruit) : Results.ValidationProblem(new Dictionary<string, string[]>
 {
   { "id", new[] { "A fruit with this id already exists" } }
-}));
+})).WithTags("fruit") 
+    .Produces<Fruit>(201) 
+    .ProducesValidationProblem();
+
 app.Run();
 record Fruit(string Name, int Stock);
