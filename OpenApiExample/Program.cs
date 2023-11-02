d5e52a19d110222a46e4bcacabdcc1326cf8cb09
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-11-02 14:30:56 +0100

    Adding OpenAPI support to a minimal API app using Swashbuckle

--- OpenApiExample/Program.cs
+++ OpenApiExample/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Collections.Concurrent;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
+WebApplication app = builder.Build();
+
+var _fruit = new ConcurrentDictionary<string, Fruit>();
+app.UseSwagger(); 
+app.UseSwaggerUI(); 
+app.MapGet("/fruit/{id}", (string id) => _fruit.TryGetValue(id, out var fruit) ? TypedResults.Ok(fruit) : Results.Problem(statusCode: 404));
+app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
+_fruit.TryAdd(id, fruit) ? TypedResults.Created($"/fruit/{id}", fruit) : Results.ValidationProblem(new Dictionary<string, string[]>
+{
+  { "id", new[] { "A fruit with this id already exists" } }
+}));
 app.Run();
+record Fruit(string Name, int Stock);
