a8f4f1a9936e1779d2e0eda218cd725d2f7f52a8
Igor Rodic <92062603+Iggy988@users.noreply.github.com>
2023-11-07 15:23:04 +0100

    Update Program.cs

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Fruit; 
+
+var client = new FruitClient("https://localhost:7186", new HttpClient()); 
+Fruit.Fruit created = await client.FruitPOSTAsync("123", new Fruit.Fruit { Name = "Banana", Stock = 100 }); 
+Console.WriteLine($"Created {created.Name}");
+Fruit.Fruit fetched = await client.FruitGETAsync("123"); 
+Console.WriteLine($"Fetched {fetched.Name}");
