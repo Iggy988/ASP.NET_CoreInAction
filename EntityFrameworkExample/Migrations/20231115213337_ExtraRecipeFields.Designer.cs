a17e67909d4eb12c10fb6bca490da76451014610
Iggy988 <igor.rodic1988@gmail.com>
2023-11-15 22:35:28 +0100

    Second migration

--- EntityFrameworkExample/Migrations/20231115213337_ExtraRecipeFields.Designer.cs
+++ EntityFrameworkExample/Migrations/20231115213337_ExtraRecipeFields.Designer.cs
@@ -4,6 +4,7 @@ using EntityFrameworkExample;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkExample.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231115213337_ExtraRecipeFields")]
+    partial class ExtraRecipeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
